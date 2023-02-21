e8656138290edf5ea171010d9804474d1236b53d
Harry Lomas <harry.lomas1@outlook.com>
2023-02-21 11:11:16 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styling/app.css';
+
+function App() {
+  
+  return(
+   <div className="Surreal Estate">
+      <h2> Surreal Estate </h2>
+    </div>  
+  );
+      
+}
+
+export default App;
